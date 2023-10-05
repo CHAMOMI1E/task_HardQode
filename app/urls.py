5315167cee929f1394993b966899f72dfdc89be0
CHAMOMILE <CHAMOM2LES@outlook.com>
2023-10-05 14:38:56 +0300

    add templates, views and urls

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', main, name='main'),
+    path('lessons/', lesson_list, name='lesson-list'),
+
+    path('product/<int:product_id>/lessons/', product_lesson_list, name='product-lesson-list'),
+
+    path('product/statistics/', product_statistics, name='product-statistics'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.contrib.auth.decorators import login_required
+
+from django.db.models import Count, Sum, Case, When, F, Value, IntegerField
 
 
-def main(request):
-    return HttpResponse("Hello World")
+from .models import *
 
-# Create your views here.
+
+@login_required
+def lesson_list(request):
+    user = request.user
+    lesson_views = LessonView.objects.filter(user=user)
+    return render(request, 'lesson_list.html', {'lesson_views': lesson_views})
+
+
+@login_required
+def product_lesson_list(request, product_id):
+    user = request.user
+    lessons = Lesson.objects.filter(products__productaccess__user=user, products__id=product_id)
+    return render(request, 'product_lesson_list.html', {'lessons': lessons})
+
+
+@login_required
+def product_statistics(request):
+    user = request.user
+    products = Product.objects.annotate(
+        total_lessons=Count('lessons'),
+        total_students=Count('productaccess'),
+        total_views=Sum(Case(
+            When(lessons__lessonview__user=user, then=F('lessons__lessonview__viewing_time')),
+            default=Value(0),
+            output_field=IntegerField()
+        )),
+        purchase_percentage=Count('productaccess', filter=models.Q(productaccess__user=user)) / Count('productaccess')
+    )
+    return render(request, 'product_statistics.html', {'products': products})
