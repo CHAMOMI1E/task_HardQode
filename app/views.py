5c2e925d566a4c74c43b3dd5362ea9272a8d2d11
CHAMOMILE <CHAMOM2LES@outlook.com>
2023-10-05 14:59:04 +0300

    add authentication_required

--- app/views.py
+++ app/views.py
@@ -1,28 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required
 
 from django.db.models import Count, Sum, Case, When, F, Value, IntegerField
 
-
 from .models import *
 
 
-@login_required
+# декоратор для проверки авторизации
+def authentication_required(view_func):
+    def wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return view_func(request, *args, **kwargs)
+
+    return wrapped_view
+
+
+@authentication_required
 def lesson_list(request):
     user = request.user
     lesson_views = LessonView.objects.filter(user=user)
     return render(request, 'lesson_list.html', {'lesson_views': lesson_views})
 
 
-@login_required
+@authentication_required
 def product_lesson_list(request, product_id):
     user = request.user
     lessons = Lesson.objects.filter(products__productaccess__user=user, products__id=product_id)
     return render(request, 'product_lesson_list.html', {'lessons': lessons})
 
 
-@login_required
+@authentication_required
 def product_statistics(request):
     user = request.user
     products = Product.objects.annotate(
